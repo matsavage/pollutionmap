71481393bf1dd2b4d16dfd8155c8719c121f026b
Mathew Savage <mathew@mathewsavage.com>
2016-10-31 14:01:47 +0000

    Updated :

--- server.R
+++ server.R
@@ -24,7 +24,9 @@ function(input, output, session) {
         urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>%
-      setView(lng = 0, lat = 51.5, zoom = 11)
+     # setView(lng = -93.85, lat = 37.45, zoom = 4) # US view
+     # setView(lng = 0, lat = 51.5, zoom = 11) #London view
+      setView(lng = 0, lat = 30, zoom = 2)
   })
   
   # A reactive expression that returns the set of zips that are
@@ -122,7 +124,7 @@ function(input, output, session) {
     if (show_SO2 == TRUE) {
       leafletProxy("map", data = so2) %>%
         #clearShapes() %>%
-        addCircles(~longitude, ~latitude, radius=~so2*15, layerId=~so2,
+        addCircles(~longitude, ~latitude, radius=~so2*150, layerId=~so2,
                    stroke=FALSE, fillOpacity=0.4, fillColor='red') #%>%
       #addCircles(~longitude, ~latitude, radius=40000, layerId=~zipcode,
       #           stroke=FALSE, fillOpacity=0.4, fillColor='blue') %>%
@@ -138,7 +140,7 @@ function(input, output, session) {
     if (show_O3 == TRUE) {
       leafletProxy("map", data = o3) %>%
         #clearShapes() %>%
-        addCircles(~longitude, ~latitude, radius=~o3*15, layerId=~o3,
+        addCircles(~longitude, ~latitude, radius=~o3*150, layerId=~o3,
                    stroke=FALSE, fillOpacity=0.4, fillColor='blue') #%>%
       #addCircles(~longitude, ~latitude, radius=40000, layerId=~zipcode,
       #           stroke=FALSE, fillOpacity=0.4, fillColor='blue') %>%
@@ -155,7 +157,7 @@ function(input, output, session) {
     if (show_PM10 == TRUE) {
       leafletProxy("map", data = pm10) %>%
         #clearShapes() %>%
-        addCircles(~longitude, ~latitude, radius=~pm10*15, layerId=~pm10,
+        addCircles(~longitude, ~latitude, radius=~pm10*150, layerId=~pm10,
                    stroke=FALSE, fillOpacity=0.4, fillColor='orange') #%>%
       #addCircles(~longitude, ~latitude, radius=40000, layerId=~zipcode,
       #           stroke=FALSE, fillOpacity=0.4, fillColor='blue') %>%
@@ -172,7 +174,7 @@ function(input, output, session) {
     if (show_PM25 == TRUE) {
       leafletProxy("map", data = pm25) %>%
         #clearShapes() %>%
-        addCircles(~longitude, ~latitude, radius=~pm25*15, layerId=~pm25,
+        addCircles(~longitude, ~latitude, radius=~pm25*150, layerId=~pm25,
                    stroke=FALSE, fillOpacity=0.4, fillColor='magenta') #%>%
       #addCircles(~longitude, ~latitude, radius=40000, layerId=~zipcode,
       #           stroke=FALSE, fillOpacity=0.4, fillColor='blue') %>%
@@ -188,7 +190,7 @@ function(input, output, session) {
     if (show_NO2 == TRUE) {
       leafletProxy("map", data = no2) %>%
         #clearShapes() %>%
-        addCircles(~longitude, ~latitude, radius=~no2*15, layerId=~no2,
+        addCircles(~longitude, ~latitude, radius=~no2*150, layerId=~no2,
                    stroke=FALSE, fillOpacity=0.4, fillColor='green') #%>%
       #addCircles(~longitude, ~latitude, radius=40000, layerId=~zipcode,
       #           stroke=FALSE, fillOpacity=0.4, fillColor='blue') %>%
